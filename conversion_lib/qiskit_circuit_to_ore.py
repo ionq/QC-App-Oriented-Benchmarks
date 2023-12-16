c032ee232988d9ee6e50fa55351c8b449c1cc393
Felix Tripier <tripier@ionq.co>
2023-12-16 16:22:16 -0500

    lil refactor

--- conversion_lib/qiskit_circuit_to_ore.py
+++ conversion_lib/qiskit_circuit_to_ore.py
@@ -2,7 +2,6 @@ from qiskit_ionq.helpers import qiskit_to_ionq
 from qiskit import transpile
 
 
-
 class LocalOrefileBackend:
     def gateset(self):
         return "qis"
@@ -11,7 +10,15 @@ class LocalOrefileBackend:
         return "local_orefile"
 
 
-def convert(qiskit_circuit, basis_gates):
-    basis_circuit = transpile(qiskit_circuit, basis_gates=basis_gates, seed_transpiler=0)
+def convert_to_ionq_circuit(qiskit_circuit, basis_gates):
+    basis_circuit = transpile(
+        qiskit_circuit, basis_gates=basis_gates, seed_transpiler=0
+    )
     ionq_circuit = qiskit_to_ionq(basis_circuit, LocalOrefileBackend())
+    return ionq_circuit
+
+
+def convert(qiskit_circuit, basis_gates):
+    ionq_circuit = convert_to_ionq_circuit(qiskit_circuit, basis_gates)
+
     return ionq_circuit
